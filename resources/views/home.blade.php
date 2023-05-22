db17b7268553be28f9a362d9c09c82deffa1e926
filecc <fil.gia@live.it>
2023-05-22 13:54:04 +0200

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.general')
+
+@section('content')
+<div>HOME</div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="container">
+        <div class="row w-100">
+            <div class="col-1">
+                <img class="img-fluid" src="images/dc-logo.png" alt="logo">
+            </div>
+        </div>
+    </div>
+</header>
