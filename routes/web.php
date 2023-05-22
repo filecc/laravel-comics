47c61dd05f703c5c82921f7b6bd983d9d33f6780
filecc <fil.gia@live.it>
2023-05-22 17:52:10 +0200

    index fixing

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/comics', function(){
 Route::get('/comic/{id}', function ($id) {
     $comics = config('comics');
 
-    if ($id > 0 && $id < count($comics)) {
+    if ($id >= 0 && $id <= count($comics)) {
         return view('comic.show', $comics[$id - 1]);
     } else {
         abort('404');
