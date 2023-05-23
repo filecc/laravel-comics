81956c9c655bdab4e58c68ce34068d0e8bce07db
filecc <fil.gia@live.it>
2023-05-23 09:12:00 +0200

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ Route::get('/comics', function(){
 Route::get('/comic/{id}', function ($id) {
     $comics = config('comics');
 
-    if ($id >= 0 && $id <= count($comics)) {
-        return view('comic.show', $comics[$id - 1]);
+    if ($id >= 0 && $id < count($comics)) {
+        return view('comic.show', $comics[$id]);
     } else {
         abort('404');
     }
